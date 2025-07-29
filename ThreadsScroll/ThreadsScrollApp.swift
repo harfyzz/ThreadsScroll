a6758a65eb3fdfebd02bd1c949e966fd6d8b94a0
Afeez <harfyzz@gmail.com>
2025-07-29 20:39:10 +0100

    Initial Commit

---
+++ ThreadsScroll/ThreadsScrollApp.swift
@@ -0,0 +1,17 @@
+//
+//  ThreadsScrollApp.swift
+//  ThreadsScroll
+//
+//  Created by Afeez Yunus on 29/07/2025.
+//
+
+import SwiftUI
+
+@main
+struct ThreadsScrollApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
